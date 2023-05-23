496cb29a7529c7a6a7d1fd8122e6b89020bb1b68
Elita1Arcee <louisa@louisajones.dev>
2023-05-22 22:55:52 -0400

    removed unused code

--- script.js
+++ script.js
@@ -6,22 +6,23 @@ let title = document.getElementById("title");
 
 // On click get name from user input and use it to update the title
 enterBtn.addEventListener("click", () => {
-    if (userName.value.length > 1) {
-        title.textContent = userName.value + "'s Animals";
-        userName.value = "";
-    }
-
-})
+  if (userName.value.length > 1) {
+    title.textContent = userName.value + "'s Animals";
+    userName.value = "";
+  }
+});
 
 //On keydown/key press of enter button, get user input and use it to update the title
 userName.addEventListener("keydown", (event) => {
-    if (userName.value.length > 1 && event.code === "Enter" || event.code === "NumpadEnter") {
-        console.log("13");
-        title.textContent = userName.value + "'s Animals";
-        userName.value = "";
-    } 
-
-})
+  if (
+    (userName.value.length > 1 && event.code === "Enter") ||
+    event.code === "NumpadEnter"
+  ) {
+    console.log("13");
+    title.textContent = userName.value + "'s Animals";
+    userName.value = "";
+  }
+});
 
 //Variables for color input and gradient container
 let color1Btn = document.getElementById("color1");
@@ -31,93 +32,75 @@ let gradientBox = document.getElementById("gradientBox");
 let randomClr = document.getElementById("randomClr");
 
 //On window load, set color gradient for background and both color inputs
-window.onload =  () => {
-    gradientBox.style.background = "linear-gradient(" + color1Btn.value + "," + color2Btn.value + ")";
-    AniCard.makeAniCard();
-    
-}
+window.onload = () => {
+  gradientBox.style.background =
+    "linear-gradient(" + color1Btn.value + "," + color2Btn.value + ")";
+  AniCard.makeAniCard();
+};
 
 //Updates the gradient container to display whatever color input by user.
 const gradientCtrl = () => {
-    gradientBox.style.background = "linear-gradient(" + color1Btn.value + "," + color2Btn.value + ")";
-}
-
+  gradientBox.style.background =
+    "linear-gradient(" + color1Btn.value + "," + color2Btn.value + ")";
+};
 
 //Function that produces a random hex color code
 const getRandomColor = () => {
-let letters = "0123456789ABCDEF";
-    let color = "#";
-    // const color2 = "#";
-    for (let i = 0; i < 6; i++) {
-        color += letters[(Math.floor(Math.random() * 16))];   
-    } 
-    return color;
-}
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
 //Function that gets two random colors for the gradient and updates the gradient container
 const colorRandomizer = () => {
-    let color1 = getRandomColor();
-    let color2 = getRandomColor();
-    
-    color1Btn.value = color1;
-    color2Btn.value = color2;
+  let color1 = getRandomColor();
+  let color2 = getRandomColor();
 
-    gradientBox.style.background = "linear-gradient(" + color1 + "," + color2 + ")";
-}
+  color1Btn.value = color1;
+  color2Btn.value = color2;
+
+  gradientBox.style.background =
+    "linear-gradient(" + color1 + "," + color2 + ")";
+};
 
 //Function that starts the process
 const getRandomColors = () => {
-    colorRandomizer();
-}
+  colorRandomizer();
+};
 
 color1Btn.addEventListener("input", gradientCtrl);
 color2Btn.addEventListener("input", gradientCtrl);
 randomClr.addEventListener("click", getRandomColors);
 
-
-
-const addAnimalBtn = document.getElementById('addAnimal');
-
-const addAniEmojiCont = () => {
-    let animalDiv = document.createElement("div");
-    animalDiv.classList.add("zone", "green");
-    let emojiAnimalAdd = document.createTextNode('🦒');
-    animalDiv.appendChild(emojiAnimalAdd);
-    return document.getElementById('allAnimalCont').appendChild(animalDiv);
-}
-
+const addAnimalBtn = document.getElementById("addAnimal");
 
 //Object contains styling class names and extra emoji animals. makeAniCard makes the card and puts it into array.
 //getCard returns one card from the array and appends it to the grid container.
 
 const AniCard = {
-    completeCard: [],
-   bgGradientColors: ['green', 'red', 'yellow', 'blue', 'purple', 'brown'],
-   addOnEmojiAnimals: ['🦒', '🐘', '🐨', '🐵', '🐶', '🐱'],
-   makeAniCard() {
-    const {completeCard, bgGradientColors, addOnEmojiAnimals} = this;
+  completeCard: [],
+  bgGradientColors: ["green", "red", "yellow", "blue", "purple", "brown"],
+  addOnEmojiAnimals: ["🦒", "🐘", "🐨", "🐵", "🐶", "🐱"],
+  makeAniCard() {
+    const { completeCard, bgGradientColors, addOnEmojiAnimals } = this;
     for (let i = 0; i < addOnEmojiAnimals.length; i++) {
-        
-        let animalDiv = document.createElement("div");
-         animalDiv.classList.add("zone", bgGradientColors[i]);
-            let emojiAnimalAdd = document.createTextNode(addOnEmojiAnimals[i]);
-            animalDiv.appendChild(emojiAnimalAdd);
-            completeCard.push(animalDiv);
-        
+      let animalDiv = document.createElement("div");
+      animalDiv.classList.add("zone", bgGradientColors[i]);
+      let emojiAnimalAdd = document.createTextNode(addOnEmojiAnimals[i]);
+      animalDiv.appendChild(emojiAnimalAdd);
+      completeCard.push(animalDiv);
     }
-    
-   },
-   getCard(){
+  },
+  getCard() {
     let newCard = this.completeCard.pop();
-    return document.getElementById('allAnimalCont').appendChild(newCard);
-   },
-
-
-    
-}
+    return document.getElementById("allAnimalCont").appendChild(newCard);
+  },
+};
 
 addAnimalBtn.addEventListener("click", () => {
-    AniCard.getCard();
+  AniCard.getCard();
 });
-
-
